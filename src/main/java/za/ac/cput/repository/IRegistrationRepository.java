a32828df27b8ef6f7ceeb33721ebb2959abbebee
Gabriel Kiewietz <160390509+Gkiewietz@users.noreply.github.com>
2025-03-22 22:16:15 +0200

    IRegistrationRepository.java

--- src/main/java/za/ac/cput/repository/IRegistrationRepository.java
+++ src/main/java/za/ac/cput/repository/IRegistrationRepository.java
@@ -1,4 +1,11 @@
 package za.ac.cput.repository;
 
-public interface IRegistrationRepository {
+import java.util.Set;
+
+public interface IRegistrationRepository<T, ID> {
+    T create(T t);
+    T read(ID id);
+    T update(T t);
+    boolean delete(ID id);
+    Set<T> getAll();
 }
