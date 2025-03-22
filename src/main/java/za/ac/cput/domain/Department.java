f862b00d7771124e326cc3dbc20e5cebff34b233
ameeruddin7 <165028003+ameeruddin7@users.noreply.github.com>
2025-03-22 14:50:31 +0200

    Update Department.java

--- src/main/java/za/ac/cput/domain/Department.java
+++ src/main/java/za/ac/cput/domain/Department.java
@@ -2,4 +2,85 @@ package za.ac.cput.domain;
 
 public class Department {
 
+  private String departmentId;
+    private String departmentName;
+    private String faculty;
+    private List<Student> students;
+    
+    private Department3() {}
+    
+    private Department3(Builder builder) {
+        this.departmentId = builder.departmentId;
+        this.departmentName = builder.departmentName;
+        this.faculty = builder.faculty;
+        this.students = builder.students;
+    }
+    
+    public String getDepartmentId() {
+        return departmentId;
+    }
+    
+    public String getDepartmentName() {
+        return departmentName;
+    }
+    
+    public String getFaculty() {
+        return faculty;
+    }
+    
+    public List<Student> getStudents() {
+        return students;
+    }
+    
+    @Override
+    public String toString() {
+        return "Department3{" +
+                "departmentId='" + departmentId + '\'' +
+                ", departmentName='" + departmentName + '\'' +
+                ", faculty='" + faculty + '\'' +
+                ", students=" + students +
+                '}';
+    }
+    
+    public static class Builder {
+        private String departmentId;
+        private String departmentName;
+        private String faculty;
+        private List<Student> students;
+        
+        public Builder setDepartmentId(String departmentId) {
+            this.departmentId = departmentId;
+            return this;
+        }
+        
+        public Builder setDepartmentName(String departmentName) {
+            this.departmentName = departmentName;
+            return this;
+        }
+        
+        public Builder setFaculty(String faculty) {
+            this.faculty = faculty;
+            return this;
+        }
+        
+        public Builder setStudents(List<Student> students) {
+            this.students = students;
+            return this;
+        }
+        
+        public Builder copy(Department3 department) {
+            this.departmentId = department.departmentId;
+            this.departmentName = department.departmentName;
+            this.faculty = department.faculty;
+            this.students = department.students;
+            return this;
+        }
+        
+        public Department3 build() {
+            return new Department3(this);
+        }
+    }
+}
+
+
 }
