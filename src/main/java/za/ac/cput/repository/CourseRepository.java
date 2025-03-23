e93cb40034b00a7db86e5cbf23edb2f98364755b
DonnaJ45 <jordaandonnalee@gmail.com>
2025-03-23 22:39:09 +0200

    updated code

--- src/main/java/za/ac/cput/repository/CourseRepository.java
+++ src/main/java/za/ac/cput/repository/CourseRepository.java
@@ -2,5 +2,67 @@
 
 package za.ac.cput.repository;
 
-public class CourseRepository {
+import za.ac.cput.domain.Course;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseRepository implements ICourseRepository {
+    private static ICourseRepository repository = null;
+    private List<Course> courseList;
+
+    private CourseRepository() { courseList = new ArrayList<>();}
+
+    public static ICourseRepository getRepository() {
+        if (repository == null) {
+            repository = new za.ac.cput.repository.CourseRepository();
+        }
+        return repository;
+    }
+    @Override
+    public Course create(Course course) {
+        boolean success = courseList.add(course);
+        if (success) {
+            return course;
+        }
+        return null;
+    }
+
+    @Override
+    public Course read(String id) {
+        for (Course course : courseList) {
+            if (course.getCourseId().equals(id)) {
+                return course;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Course update(Course course) {
+        String id = course.getCourseId();
+        Course oldCourse = read(id);
+        if (oldCourse == null) {
+            return null;
+        }
+
+        boolean success = delete(id);
+        if (success) {
+            courseList.add(course);
+            return course;
+        }
+        return null;
+    }
+    @Override
+    public boolean delete(String id) {
+        Course courseToDelete = read(id);
+        if (courseToDelete == null) {
+            return false;
+        }
+        return courseList.remove(courseToDelete);
+    }
+
+    @Override
+    public List<Course> getAll() {
+        return this.courseList;
+    }
 }
--- src/test/java/za/ac/cput/factory/CourseFactoryTest.java
+++ src/test/java/za/ac/cput/factory/CourseFactoryTest.java
@@ -2,5 +2,48 @@
 
 package za.ac.cput.factory;
 
-public class CourseFactoryTest {
+import org.junit.jupiter.api.*;
+import za.ac.cput.domain.Course;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class CourseFactoryTest {
+
+    private static Course c1 = CourseFactory.createCourse("2023001", "Application Development", 50, null, null, null);
+    private static Course c2 = CourseFactory.createCourse("2023002", "Project Management", 40, null, null, null);
+    private static Course c3 = CourseFactory.createCourse("2023003", "Professional Communication", 30, null, null, null);
+
+
+    @Test
+    @Order(1)
+    public void testCreateCourse() {
+        assertNotNull(c1);
+        System.out.println(c1.toString());
+    }
+
+
+    @Test
+    @Order(2)
+    public void testCreateCourseWithAllAttributes() {
+        assertNotNull(c2);
+        System.out.println(c2.toString());
+    }
+
+
+    @Test
+    @Order(3)
+    public void testCreateCourseThatFails() {
+        // This test is expected to fail if the factory validation fails
+        Course c3 = CourseFactory.createCourse("2085489", "Information Technology", 30, null, null, null);
+        assertNotNull(c3); // This will fail intentionally
+        System.out.println(c3.toString());
+    }
+
+
+    @Test
+    @Order(4)
+    @Disabled
+    public void testNotImplementedYet() {
+        //Todo
+    }
 }
