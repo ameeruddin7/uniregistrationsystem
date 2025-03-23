104610e19a809884df1d4a2d481fc0d49b28443c
Gabriel Kiewietz <160390509+Gkiewietz@users.noreply.github.com>
2025-03-23 14:40:20 +0200

    RegistrationFactory.java

--- src/main/java/za/ac/cput/factory/RegistrationFactory.java
+++ src/main/java/za/ac/cput/factory/RegistrationFactory.java
@@ -7,12 +7,11 @@ package za.ac.cput.factory;
 import za.ac.cput.domain.Registration;
 import za.ac.cput.domain.Student;
 import za.ac.cput.domain.Course;
-import za.ac.cput.util.Helper;
 import java.time.LocalDate;
 
 public class RegistrationFactory {
     public static Registration createRegistration(String registrationID, Student student, Course course, LocalDate registrationDate) {
-        if (Helper.isNullOrEmpty(registrationID) || student == null || course == null || registrationDate == null)
+        if (registrationID == null || registrationID.isEmpty() || student == null || course == null || registrationDate == null)
             return null;
 
         return new Registration.Builder()
