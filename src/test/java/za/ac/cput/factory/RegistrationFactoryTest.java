386f3104033cf1ddc377359849942db5d05336bd
Gabriel Kiewietz <160390509+Gkiewietz@users.noreply.github.com>
2025-03-26 11:12:18 +0200

    RegistrationFactoryTest.java

--- src/test/java/za/ac/cput/factory/RegistrationFactoryTest.java
+++ src/test/java/za/ac/cput/factory/RegistrationFactoryTest.java
@@ -1,36 +1,63 @@
 package za.ac.cput.factory;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 import za.ac.cput.domain.Registration;
 import za.ac.cput.domain.Student;
 import za.ac.cput.domain.Course;
-import za.ac.cput.factory.RegistrationFactory;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-        public class RegistrationFactoryTest {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class RegistrationFactoryTest {
 
-        @Test
-        void createRegistration_success() {
-            Student student = new Student("5555", "Dave", "Bakes");
-            Course course = new Course("ITC", "Introduction to Course");
-            LocalDate registrationDate = LocalDate.now();
+    private static Student student1;
+    private static Student student2;
+    private static Course course1;
+    private static Course course2;
 
-            Registration registration = RegistrationFactory.createRegistration("007reg", student, course, registrationDate);
+    private static Registration reg1;
+    private static Registration reg2;
+    private static Registration reg3;
 
-            assertNotNull(registration);
-            assertEquals("007reg", registration.getRegistrationID());
-            assertEquals(student, registration.getStudent());
-            assertEquals(course, registration.getCourse());
-            assertEquals(registrationDate, registration.getRegistrationDate());
-        }
+    @BeforeAll
+    static void setUp() {
+        student1 = new Student("2023001", "Gabriel", "Kiewietz");
+        student2 = new Student("2023002", "Ameer", "Arai");
+        course1 = new Course("IT101", "Introduction to IT");
+        course2 = new Course("CS202", "Data Structures");
 
-        @Test
-        void createRegistration_fail_nullValues() {
-            Registration registration = RegistrationFactory.createRegistration(null, null, null, null);
-            assertNull(registration);
-        }
+        reg1 = RegistrationFactory.createRegistration("REG001", student1, course1, LocalDate.of(2024, 3, 1));
+        reg2 = RegistrationFactory.createRegistration("REG002", student2, course2, LocalDate.of(2024, 3, 5));
+        reg3 = RegistrationFactory.createRegistration("REG003", student1, course2, LocalDate.of(2024, 3, 10));
     }
 
+    @Test
+    @Order(1)
+    public void testCreateRegistration() {
+        assertNotNull(r1);
+        System.out.println(r1.toString());
+    }
+
+    @Test
+    @Order(2)
+    public void testCreateRegistrationWithAllAttributes() {
+        assertNotNull(r2);
+        System.out.println(r2.toString());
+    }
+
+    @Test
+    @Order(3)
+    public void testCreateRegistrationThatFails() {
+        Registration r4 = RegistrationFactory.createRegistration(null, null, null, null);
+        assertNull(r4);
+    }
+
+    @Test
+    @Order(4)
+    @Disabled
+    public void testNotImplementedYet() {
+        // Todo: Add future test cases
+    }
 }
